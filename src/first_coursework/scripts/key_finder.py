52426828751071795a9048ddef9cfc57ffb56486
Nourah <nourah.al_hassan@kcl.ac.uk>
2024-10-30 18:23:15 +0000

    itr day 3

--- src/first_coursework/scripts/key_finder.py
+++ src/first_coursework/scripts/key_finder.py
@@ -1,13 +1,56 @@
-#!/usr/bin/env/ python3
+#!/usr/bin/env python3
 import rospy
-from math import *
+import math
+import angles
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
-from std_msgs.msg import Float64, String
+from std_msgs.msg import Float64, String, Float32
+from first_coursework.msg import Intention
 
-rospy.init_node('beacon_node', Anonymous = True)
+rospy.init_node('key_finder', anonymous = True)
+cmd_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 10)
+rate = rospy.Rate(10)
 
-cmd_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 100)
-current_pose = Pose()
-pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, dist_pub)
+pose = None
+beacon_position = None
+
+def getDistance(dist: Float32):
+    global distance_to_beacon
+    distance_to_beacon = dist.data
+
+def getPose(pose_msg: Pose):
+    global pose
+    pose = pose_msg
+
+
+def moveTurtle():
+    global pose, beacon_position
+
+    if pose is not None and beacon_position is not None:
+        msg = Twist()
+
+    diff = (beacon_position[0] - pose.x, beacon_position[1] - pose.y)
+    beacon_position = math.atan2(diff[1], diff[0])
+
+    angle_diff = angles.shortest_angular_distance(pose.theta, beacon_position)
+
+    if angle_diff < -0.087 or angle_diff > 0.087:
+        msg.angular.z = -1
+    elif math.hypot(*diff) > 0.3:
+        msg.linear.x = 1
+
+    cmd_pub.publish(msg)
+
+beacon_sub = rospy.Subscriber('/beacon', Float32, getDistance)
+pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, getPose)
+
+while not rospy.is_shutdown():
+    moveTurtle()
+    rate.sleep()
+
+
+
+
+
+
 
